197add9b73e32ea7e152543888fa89914f42840a
Benjamin Labrecque <labrecque.benji@gmail.com>
2023-01-29 11:55:13 +0100

    Add poetry.toml and poetry.mk

---
+++ poetry.mk
@@ -0,0 +1,52 @@
+APP_DIR ?= $(shell pwd)
+PYTHON ?= 3.8
+PYTESTARGS ?=
+POETRY_VENV = .poetry_venv
+POETRY ?= ${POETRY_VENV}/bin/poetry
+
+${POETRY_VENV}:
+	python -m venv ${POETRY_VENV}
+	${POETRY_VENV}/bin/pip install pip -U
+	${POETRY_VENV}/bin/pip install poetry 
+	${POETRY} config virtualenv.in-project true
+	${POETRY} config virtualenv.create true
+
+.LOW_RESOLUTION_TIME: poetry.lock pyproject.toml
+poetry.lock: pyproject.toml | ${POETRY_VENV}
+	${POETRY} lock
+	touch poetry.lock
+
+.venv: poetry.lock | ${POETRY_VENV}
+	${POETRY} install --remove-untracked
+
+env: | .venv
+	ln -s .venv env
+
+.PHONY: develop
+develop: env
+
+.PHONY: install
+install: develop
+
+.PHONY: clean
+clean:
+	rm -rf ${POETRY_VENV} env .venv .cache *.egg-info dist build coverage.xml unittest_results.xml .coverage
+	find . -type -name \*.pyc -delete
+
+.PHONY: mypy
+mypy: develop
+	${POETRY} run mypy
+
+.PHONY: test
+test: develop
+	${POETRY} run pytest \
+	--cov=${APP_DIR} \
+	--cov-report=term \
+	--cov-report=xml:coverage.xml \
+	junit-xml=unittest.xml \
+	${PYTESTARGS}
+
+.PHONY: wheel
+wheel: ${POETRY_VENV}
+	${POETRY} build
+
